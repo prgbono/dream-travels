e1c4580d3d321499cb402858e00fcb4f94f59261
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-19 16:32:23 +0200

    chore: :clown_face: Mock data

---
+++ app/mock/TripMocked.js
@@ -0,0 +1,23 @@
+export const tripMocked = {
+  id: 1,
+  title: 'Portugal',
+  description:
+    'Embark on a journey through Portugal, where the charming streets of Lisbon captivate you, the golden beaches of the Algarve await, and Portuguese cuisine delights with authentic flavors. Explore castles in Sintra and create unforgettable memories in this destination full of history and beauty. Portugal invites you to experience something truly unique!',
+  photo_url:
+    'https://a.cdn-hotels.com/gdcs/production82/d1923/447a348f-f875-4885-b00a-e9a90603fef5.jpg',
+  status: 'todo',
+  itinerary: [
+    {
+      day: 1,
+      location: 'Lisbon',
+      description: 'Explore the Alfama neighborhood and visit São Jorge Castle.'
+    },
+    {
+      day: 2,
+      location: 'Lisbon',
+      description:
+        'Visit the Jerónimos Monastery and the Monument to the Discoveries.'
+    }
+  ],
+  completed: false
+}
