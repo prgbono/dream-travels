7b096a503ec1ee010bcfe9a67af1335381e2faaf
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-22 07:43:53 +0200

    feat: :sparkles: Set filter for trips

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { Footer, Header } from './ui'
 import { TripList } from './components'
 import { fetchTrips } from './api'
 import { Trip } from './types'
-import NavButton from './ui/NavButton'
 import { NAVBAR_BUTTONS_TEXT } from './utils/constants'
 import NavBar from './ui/NavBar'
 
 const HomePage: React.FC = () => {
   const [trips, setTrips] = useState<Trip[]>([])
+  const [filter, setFilter] = useState<string>('')
 
 import Image from "next/image";
 
@@ -19,9 +19,14 @@ export default function Home() {
     fetchTrips().then(setTrips)
   }, [])
 
+  const filteredTrips = trips.filter(
+    (trip) =>
+      trip.title.toLowerCase().includes(filter.toLowerCase()) ||
+      trip.description.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
-    <div>
+    <>
       <Header />
       <main className="min-h-screen py-12 text-center">
         <div className="mb-4 text-center">
@@ -29,94 +34,12 @@ export default function Home() {
           <p className="text-2xl text-gray-600">Let’s live new adventures</p>
         </div>
 
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
         <div className="relative w-full max-w-md mx-auto mb-8">
           <input
             type="text"
             placeholder="Search trips"
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
             className="w-full p-4 border rounded-full"
           />
           <button className="absolute px-4 py-2 text-white transform -translate-y-1/2 bg-black rounded-full right-2 top-1/2">
@@ -127,10 +50,13 @@ export default function Home() {
         <nav className="flex justify-center mb-12">
           <NavBar buttonsText={NAVBAR_BUTTONS_TEXT} />
         </nav>
+        <TripList
+          trips={filteredTrips}
         <TripList trips={filteredTrips} />
       </main>
       <Footer />
     </div>
+    </>
   )
 }
 
