532adb3ddf7fb5ddbe673a4423f103a1e48d26b5
Paco Ríos <pacoriosgalan@gmail.com>
2024-07-24 15:08:20 +0200

    feat: :dizzy: Allow pictures from any domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: [
-      'my-json-server.typicode.com',
-      'a.cdn-hotels.com',
-      'cdn.aarp.net',
-      'content.skyscnr.com',
-      'humanidades.com',
-      'cdn.britannica.com',
-      'lp-cms-production.imgix.net',
-      'miro.medium.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      },
+      {
+        protocol: 'http',
+        hostname: '**'
+      }
     ]
   }
 }
